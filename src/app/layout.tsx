23d2d4661383bf6d8e53f68e7b9912cb27ca6029
MGSimard <g.marcgs@gmail.com>
2024-09-11 20:32:25 -0400

    Meta stuff and favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,46 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import Link from "next/link";
 import { Taskbar } from "@/components/Taskbar";
-
 import { Handjet } from "next/font/google";
 import "@/styles/globals.css";
 
 const handjet = Handjet({ subsets: ["latin"] });
 
+export const viewport: Viewport = {
+  themeColor: "#2a5ed2",
+};
+
 export const metadata: Metadata = {
-  title: "VertexBlog",
-  description: "This is a terrible good idea",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: "Vertex Blog",
+  description: "Greatest bad idea I've ever had",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicons/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicons/favicon-32x32.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicons/apple-touch-icon.png",
+    },
+    {
+      rel: "mask-icon",
+      color: "#f75049",
+      url: "/favicons/safari-pinned-tab.svg",
+    },
+  ],
+  manifest: "/favicons/site.webmanifest",
+  other: {
+    "msapplication-TileColor": "#2a5ed2",
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
