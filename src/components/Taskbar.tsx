ce93fbff469009aab3fbf18e40bee486f935d592
MGSimard <g.marcgs@gmail.com>
2024-09-12 14:18:53 -0400

    No longer a mystery I did an oopsie

--- src/components/Taskbar.tsx
+++ src/components/Taskbar.tsx
@@ -3,7 +3,7 @@ import { StartButton } from "./StartButton";
 export function Taskbar() {
   return (
     <div id="taskbar" className="noselect">
-      <StartButton /> TASK BARTASK BARTASK BARTASK BARTASK BARTASK BAR
+      <StartButton /> <span>TASK BARTASK BARTASK BARTASK BARTASK BARTASK BAR</span>
     </div>
   );
 }
