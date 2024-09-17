4674f68f7ac96695eeab2506be3e40c9c8dff172
MGSimard <g.marcgs@gmail.com>
2024-09-17 03:40:12 -0400

    Window focus swaps on click finished(?). A bit janky, so I included full comment at the start of Notepad.tsx file.
    Basically, base z-index is
    .window -> .notepad -> maximized .window -> .active-window
    
    Clicking any window sets it as .active-window (z-index: 903), this is good however problematic when clicking a notepad file - this sets the file explorer/documents window as .active-window, which means the then rendering notepad rendering below.
    
    As a fix, when a notepad instance renders we just clear .active-window from every .window element, that way notepad's explicit z-index 901 takes over and renders on top of the file explorer. It will also render on top of older notepad windows due to hierarchy.
    
    I also accounted for the fact that I might allow focusing windows on top of maximized windows later on, so .active-window renders on top of .maximized windows.

--- src/components/DragProvider.tsx
+++ src/components/DragProvider.tsx
@@ -8,11 +8,20 @@ export function DragProvider({ children }: { children: React.ReactNode }) {
 
   const handleMouseDown = (e: MouseEvent) => {
     const target = e.target as HTMLElement;
+    // If target is a drag control, start dragging closest draggable parent
     if (target.dataset.dragcontrol) {
       dragEleRef.current = target.closest('[data-draggable="true"]') as HTMLElement;
       const { x: eleX, y: eleY } = dragEleRef.current.getBoundingClientRect();
       setDragOffset({ x: e.clientX - eleX, y: e.clientY - eleY });
       setDragging(true);
+    }
+    // This is for .window focus on click (shifts z-index)
+    // If target is .window or descendant of .window, clear .active-window class from all .window
+    // then set .active-window on the target window (z-index: 903, render on top)
+    if (target.closest(".window")) {
+      const windows = document.querySelectorAll(".window");
+      windows.forEach((window) => window.classList.remove("active-window"));
+      target.closest(".window")!.classList.add("active-window");
     }
   };
 
