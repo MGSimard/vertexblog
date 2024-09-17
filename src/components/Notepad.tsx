a65a03676241d3013b2ed79aa6ae4b987909350a
MGSimard <g.marcgs@gmail.com>
2024-09-17 05:24:05 -0400

    Have to test something on live prod

--- src/components/Notepad.tsx
+++ src/components/Notepad.tsx
@@ -7,6 +7,7 @@ import { useZIndex } from "@/lib/zIndexProvider";
 
 export function Notepad({ postInfo, onClose }: { postInfo: PostInfoTypes; onClose: () => void }) {
   const { zIndex, incrementZIndex } = useZIndex();
+
   /** Bit of context regarding window focus here:
    * When we open notepad, we clicked in file explorer which had set .active-window, so we clear it
    * to ensure new notepad renders on top. Technically this happens without also setting .active-window
@@ -21,9 +22,16 @@ export function Notepad({ postInfo, onClose }: { postInfo: PostInfoTypes; onClos
 
   const notepadRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
+    console.log("Notepad mounted.");
+    console.log(zIndex);
+    incrementZIndex();
     const windows = document.querySelectorAll(".window");
     windows.forEach((window) => window.classList.remove("active-window"));
     notepadRef.current!.classList.add("active-window");
+
+    return () => {
+      console.log("Notepad unmounted.");
+    };
   }, []);
 
   return (
