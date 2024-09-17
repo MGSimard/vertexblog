05fccb43363904d1b3a3afb17a0186a19f763bdc
MGSimard <g.marcgs@gmail.com>
2024-09-17 03:53:51 -0400

    Typo

--- src/components/Notepad.tsx
+++ src/components/Notepad.tsx
@@ -6,13 +6,13 @@ import { CloseIcon } from "./icons";
 export function Notepad({ postInfo, onClose }: { postInfo: PostInfoTypes; onClose: () => void }) {
   /**
    * When we mount a new notepad instance, remove .active-window class (z-index: 903)
-   * from every window in the DOM. (When we open a notepad, the click event sets .active-window
+   * from every .window in the DOM. (When we open a notepad, the click event sets .active-window
    * on the file explorer window where the notepad shortcut was, which would lead to spawning
    * the notepad window behind it. Due to this, we clear .active-window from everywhere to ensure
    * the new notepad instance will be on top (due to z-index: 901, and due to DOM hierarchy against
    * older notepad windows).
    *
-   * From there, a set of unstructions in DragProvider.tsx's click event function handles setting
+   * From there, a set of instructions in DragProvider.tsx's click event function handles setting
    * .active-window to any window we click into, giving it the highest z-index and simulating focus.
    * A maximized window will also have 902 z-index by default (both file explorer and notepads) in
    * order to render over other notepads - unless we then swap focus by making another window .active-focus.
