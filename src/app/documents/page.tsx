d5c691947cf5a2c6e8ead41f887338c8c728ffe8
MGSimard <g.marcgs@gmail.com>
2024-09-22 09:40:54 -0400

    Thinking about potentially removing menu / exit option from notepad and just have a save icon instead, exit button is redundant when you already have close window button top right (although argument could be made file > save, file > exit is faster flow on tab targetting

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -2,16 +2,14 @@ import { CurrentPath } from "@/components/CurrentPath";
 import { CreateBlogForm } from "@/components/CreateBlogForm";
 import { BlogList } from "@/components/BlogList";
 
-export default async function Page() {
+export default function Page() {
   return (
     <>
       <div className="window-options winbtns">
         <button type="button">File</button>
-        <button type="button">Edit</button>
-        <button type="button">Go</button>
-        <button type="button">Favorites</button>
+        <button type="button">View</button>
+        <button type="button">Sort</button>
         {/* SHOW USER'S FAVORITE BLOGS? */}
-        <button type="button">Help</button>
       </div>
       <div className="window-address">
         <span>Address</span>
