d2dfeb9b769be94c87f67b05790e5aa8e8d79423
MGSimard <g.marcgs@gmail.com>
2024-09-22 11:48:06 -0400

    Cleanup

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -8,7 +8,7 @@ export async function BlogList() {
   // PROBABLY FILE SORTING
 
   return (
-    <ul className="shortcut-area" id="documents-iconarea">
+    <ul className="shortcut-area">
       {/* Temporary error message */}
       {!success && message}
       {data &&
