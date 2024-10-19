b639e3166c323d0302a5fb5964656efab6aa829d
MGSimard <g.marcgs@gmail.com>
2024-10-19 13:19:08 -0400

    Disclaimer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                     <span>Documents</span>
                   </Link>
                 </li>
+                <li>This app is in active development.</li>
               </ul>
               <Taskbar />
             </nav>
