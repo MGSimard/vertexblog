d98a250e933d172e160d30f0dccb713980b486c9
MGSimard <g.marcgs@gmail.com>
2024-10-23 00:07:34 -0400

    Put this back for a bit while I work on virt

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -14,8 +14,8 @@ export function BlogList({ blogList }: { blogList: GetBlogsResponseTypes }) {
   // TODO: Clean this shit up once you have a functioning feature
 
   const containerRef = useRef<HTMLDivElement>(null);
-  const [containerWidth, setContainerWidth] = useState(0);
-  const [scrollPos, setScrollPos] = useState(0);
+  // const [containerWidth, setContainerWidth] = useState(0);
+  // const [scrollPos, setScrollPos] = useState(0);
 
   const sortedBlogs = data?.sort((a, b) => {
     switch (blogSortType) {
@@ -33,57 +33,57 @@ export function BlogList({ blogList }: { blogList: GetBlogsResponseTypes }) {
     }
   });
 
-  const itemCount = 3000;
-  const itemWidth = 80;
-  const itemHeight = 80;
-  const gap = 30;
-  const bufferSize = 3;
+  // const itemCount = 3000;
+  // const itemWidth = 80;
+  // const itemHeight = 80;
+  // const gap = 30;
+  // const bufferSize = 3;
 
-  const columns = Math.floor((containerWidth + gap) / (itemWidth + gap));
-  const rows = Math.ceil(itemCount / columns);
+  // const columns = Math.floor((containerWidth + gap) / (itemWidth + gap));
+  // const rows = Math.ceil(itemCount / columns);
 
-  useEffect(() => {
-    if (!containerRef.current) return;
-    const observer = new ResizeObserver(([ele]) => {
-      if (ele?.target instanceof HTMLElement) {
-        setContainerWidth(ele.target.clientWidth);
-      }
-    });
-    observer.observe(containerRef.current);
-    setContainerWidth(containerRef.current.clientWidth);
-    // remove bg later
-    containerRef.current.style.background = "red";
-    return () => observer.disconnect();
-  }, []);
+  // useEffect(() => {
+  //   if (!containerRef.current) return;
+  //   const observer = new ResizeObserver(([ele]) => {
+  //     if (ele?.target instanceof HTMLElement) {
+  //       setContainerWidth(ele.target.clientWidth);
+  //     }
+  //   });
+  //   observer.observe(containerRef.current);
+  //   setContainerWidth(containerRef.current.clientWidth);
+  //   // remove bg later
+  //   containerRef.current.style.background = "red";
+  //   return () => observer.disconnect();
+  // }, []);
 
-  useEffect(() => {
-    if (sortedBlogs && containerWidth && containerRef.current) {
-      const virtualHeight = rows * (itemHeight + gap) - gap;
-      containerRef.current.style.height = `${virtualHeight / 10}rem`;
-    }
-  }, [containerWidth]);
+  // useEffect(() => {
+  //   if (sortedBlogs && containerWidth && containerRef.current) {
+  //     const virtualHeight = rows * (itemHeight + gap) - gap;
+  //     containerRef.current.style.height = `${virtualHeight / 10}rem`;
+  //   }
+  // }, [containerWidth]);
 
-  /** VIRTUALIZATION PLAN:
-   * - Scroll container: Position relative
-   * - Group items (flex subcontainer, width 100%)
-   * - Make group absolute position 0,0, then translate Y using first item's intended position using its index
-   * (Basically just offset Y by amount of previous rows (row height + gap))
-   * - First item should always be the matching item on the leftmost of the row, last item rightmost of last row
-   * - Could decide a flat amount of items to render, could also calc how many should be rendered according to
-   * container visible height and pad the top and bottom for some level of preloading like in old tilebased RPGs
-   * - Get relevant index of first object to render according to current scroll position (dunno how yet)
-   */
+  // /** VIRTUALIZATION PLAN:
+  //  * - Scroll container: Position relative
+  //  * - Group items (flex subcontainer, width 100%)
+  //  * - Make group absolute position 0,0, then translate Y using first item's intended position using its index
+  //  * (Basically just offset Y by amount of previous rows (row height + gap))
+  //  * - First item should always be the matching item on the leftmost of the row, last item rightmost of last row
+  //  * - Could decide a flat amount of items to render, could also calc how many should be rendered according to
+  //  * container visible height and pad the top and bottom for some level of preloading like in old tilebased RPGs
+  //  * - Get relevant index of first object to render according to current scroll position (dunno how yet)
+  //  */
 
-  useEffect(() => {
-    const scrollEle = containerRef.current?.parentElement;
-    if (scrollEle) {
-      scrollEle.addEventListener("scroll", () => console.log("SCROLL POS:", scrollEle.scrollTop));
+  // useEffect(() => {
+  //   const scrollEle = containerRef.current?.parentElement;
+  //   if (scrollEle) {
+  //     scrollEle.addEventListener("scroll", () => console.log("SCROLL POS:", scrollEle.scrollTop));
 
-      return () => {
-        scrollEle.removeEventListener("scroll", () => console.log("SCROLL POS:", scrollEle.scrollTop));
-      };
-    }
-  }, [containerRef]);
+  //     return () => {
+  //       scrollEle.removeEventListener("scroll", () => console.log("SCROLL POS:", scrollEle.scrollTop));
+  //     };
+  //   }
+  // }, [containerRef]);
 
   return (
     <div ref={containerRef} className="scroll-container">
@@ -91,16 +91,14 @@ export function BlogList({ blogList }: { blogList: GetBlogsResponseTypes }) {
       {/* Temporary error message */}
       {!success && message}
       <ul className={`shortcut-area view-${iconView}`}>
-        {Array.from({ length: 1000 }).map((_, index) =>
-          sortedBlogs?.map((blog) => (
-            <li key={`${blog.blogId}-${index}`}>
-              <Link href={`/documents/${encodeURIComponent(blog.blogTitle)}`} className="shortcut">
-                <img src={`/assets/${blog.active ? "FilledFolder" : "EmptyFolder"}.svg`} alt="Folder" />
-                <span>{blog.blogTitle}</span>
-              </Link>
-            </li>
-          ))
-        )}
+        {sortedBlogs?.map((blog) => (
+          <li key={blog.blogId}>
+            <Link href={`/documents/${encodeURIComponent(blog.blogTitle)}`} className="shortcut">
+              <img src={`/assets/${blog.active ? "FilledFolder" : "EmptyFolder"}.svg`} alt="Folder" />
+              <span>{blog.blogTitle}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
