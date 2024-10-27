743b081bd767f11955801595e739ac9d486fb372
MGSimard <g.marcgs@gmail.com>
2024-10-27 01:59:55 -0400

    Work on some sysinfo

--- src/components/DxDiag.tsx
+++ src/components/DxDiag.tsx
@@ -38,7 +38,83 @@ export function DxDiag() {
               </button>
             </div>
           </div>
-          <div>WINDOW CONTENT</div>
+          <div className="system-content">
+            <h1>System Information</h1>
+            <div className="system-container">
+              <h2>Core Technologies</h2>
+              <ul>
+                <li>
+                  <a href="https://nextjs.org/" target="_blank">
+                    Next.js 15
+                  </a>
+                </li>
+                <li>
+                  <a href="https://react.dev" target="_blank">
+                    React 19 RC
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.typescriptlang.org/" target="_blank">
+                    TypeScript 5.6.3
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.postgresql.org/" target="_blank">
+                    PostgreSQL
+                  </a>
+                </li>
+                <li>
+                  <a href="https://vercel.com/" target="_blank">
+                    Vercel Hosting
+                  </a>
+                </li>
+                <li>
+                  <a href="https://create.t3.gg/" target="_blank">
+                    Initially based on T3 Stack
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="system-container">
+              <h2>Libraries</h2>
+              <table className="system-table">
+                <tbody>
+                  <tr>
+                    <th>Authentication:</th>
+                    <td>
+                      <a href="https://lucia-auth.com/" target="_blank">
+                        Lucia Auth
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>ORM:</th>
+                    <td>
+                      <a href="https://orm.drizzle.team/" target="_blank">
+                        Drizzle ORM
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Validation:</th>
+                    <td>
+                      <a href="https://zod.dev/" target="_blank">
+                        Zod
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>Hashing</th>
+                    <td>
+                      <a href="https://www.npmjs.com/package/@node-rs/argon2" target="_blank">
+                        Argon2
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </WindowFrame>
       </Modal>
     </>
