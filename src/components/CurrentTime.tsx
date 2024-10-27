167ca0264d086116c0a79b25c64522d1a0ef31a5
MGSimard <g.marcgs@gmail.com>
2024-10-26 22:00:11 -0400

    lil mf

--- src/components/CurrentTime.tsx
+++ src/components/CurrentTime.tsx
@@ -2,21 +2,25 @@
 import { useState, useEffect } from "react";
 
 export function CurrentTime() {
-  const [time, setTime] = useState<Date>(new Date());
+  const [time, setTime] = useState<string>("00:00");
 
   useEffect(() => {
-    const updateTime = () => setTime(new Date());
+    const updateTime = () => {
+      const utcDate = new Date();
+      const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone ?? "UTC";
+      setTime(
+        utcDate.toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "2-digit",
+          timeZone: userTimeZone,
+        })
+      );
+    };
+    updateTime();
+
     const secondInterval = setInterval(updateTime, 1000);
     return () => clearInterval(secondInterval);
   }, []);
 
-  return (
-    <div className="current-time inset">
-      {time.toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "2-digit",
-        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-      })}
-    </div>
-  );
+  return <div className="current-time inset">{time}</div>;
 }
